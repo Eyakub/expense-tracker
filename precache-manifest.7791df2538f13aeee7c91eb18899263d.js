8b28fcbd48a38212dcb07719232190dc3460e24d
eyakub <eyakubsorkar@gmail.com>
2020-02-28 03:56:49 +0600

    Updates

---
+++ precache-manifest.7791df2538f13aeee7c91eb18899263d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e205832a3d7d681472d6a7aa457514",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "342b952b1ae007177790",
+    "url": "/expense-tracker/static/css/main.a2a8704d.chunk.css"
+  },
+  {
+    "revision": "6c4a503a07e7c39835e6",
+    "url": "/expense-tracker/static/js/2.fe0fce3d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/expense-tracker/static/js/2.fe0fce3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342b952b1ae007177790",
+    "url": "/expense-tracker/static/js/main.0fc5e1e5.chunk.js"
+  },
+  {
+    "revision": "59a71736c99d25fe4e33",
+    "url": "/expense-tracker/static/js/runtime-main.e1909f45.js"
+  }
+]);
